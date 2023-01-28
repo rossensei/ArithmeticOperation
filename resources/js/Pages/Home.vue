0793fc219e3d764e71f1ac5a9a5b1cbee0418531
rossensei <fross0988@gmail.com>
2023-01-28 23:46:41 +0800

    initial

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row mt-5">
+        <div class="col-md-4 mx-auto">
+            <div class="card ">
+                <h3 class="text-center bg-primary rounded">Modulo Calculator</h3>
+
+                <form class="form-horizontal mx-auto" style="margin-top: 25px">
+                    <div class="form-group d-flex mb-3">
+                        <label for="firstnum" class="col-sm-2 control-label">a =</label>
+                        <div class="col-md-8">
+                            <input type="number" class="form-control" id="firstnum">
+                        </div>
+                    </div>
+                    <div class="form-group d-flex mb-3">
+                        <label for="secondnum" class="col-sm-2 control-label">b =</label>
+                        <div class="col-md-8">
+                            <input type="number" class="form-control" id="secondnum">
+                        </div>
+                    </div>
+                    <div class="form-group d-flex mb-3">
+                        <div class="col-md-8">
+                            <div class="btn btn-primary">Calculate</div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    @vite('resources/css/app.css')
+    @inertiaHead
+  </head>
+  <body>
+    @inertia
+    @vite('resources/js/app.js')
+  </body>
+</html>
